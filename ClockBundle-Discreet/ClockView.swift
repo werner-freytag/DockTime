d98007c86dc697656497fc8b0a378a826af82e10
Werner Freytag <freytag@gmx.de>
2021-02-18 17:08:38 +0100

    update Copyrights

--- ClockBundle-Discreet/ClockView.swift
+++ ClockBundle-Discreet/ClockView.swift
@@ -1,6 +1,6 @@
 // The MIT License
 //
-// Copyright 2012-2019, 2021 Werner Freytag
+// Copyright 2012-2021 Werner Freytag
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
